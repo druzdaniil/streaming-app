212380f6ca24791995be851402b85cc91a9245e6
druzdaniil <danyadruz2006@gmail.com>
2026-05-12 00:11:54 +0300

    code formatting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+   server: {
+      port: 5173,
+      proxy: {
+         "/api": {
+            target: "http://localhost:3000",
+            changeOrigin: true,
+         },
+      },
+   },
+});
