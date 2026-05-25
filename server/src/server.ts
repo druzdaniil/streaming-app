67b50777cdee8ee3276f6c5ec36f5b0c52ad9696
druzdaniil <danyadruz2006@gmail.com>
2026-05-25 23:54:17 +0300

    connect dotenv package

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
-import 'dotenv/config';
+import "dotenv/config";
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
    console.log(`Сервер запущено: http://localhost:${PORT}`);
-   console.log(`Тест: http://localhost:${PORT}/api/health`);
 });
